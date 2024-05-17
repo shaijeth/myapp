8c2dfa27329faff03b56921623b31a5eaf8ca112
shaijeth <shailendra.jethiwal@gmail.com>
2024-05-17 11:28:40 +0530

    toggle btn

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+//   <script>
+//   const toggleBtn = document.querySelector('.toggle_btn')
+//   const toggleBtnIcon = document.querySelector('.toggle_btn i')
+//   const dropDownMenu = document.querySelector('.dropdown_menu')
+//   toggleBtn.onclick= function(){
+//       dropDownMenu.classList.toggle('open')
+//       const isopen= dropDownMenu.classList.contains('open')
+//       toggleBtnIcon.classList = isopen ? 'fa fa-close' :'fa fa-bars'
+//   }
+// </script>
+
+isopen:boolean=false; 
+clikevent(){
+  this.isopen=!this.isopen;
+}
+
 }
