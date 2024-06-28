0931a3374067a6e33fd7b09ebc291316cf4bb4f6
shaijeth <shailendra.jethiwal@gmail.com>
2024-06-28 12:00:43 +0530

    mponline and services css  send to styls css

--- src/app/school/school.component.ts
+++ src/app/school/school.component.ts
@@ -12,12 +12,6 @@ import { Iservice } from '../../assets/model/Iservice';
 export class SchoolComponent {
 
 
-  servicesicon:Iservice[]= [{icon:"1",file:"1.pdf",desc:"Nice to Meet You"},
-   {icon:"2",file:"1.pdf",desc:"Nice 2 to Meet You"},
-    {icon:"3",file:"1.pdf",desc:"Nice 3to Meet You"},
-    {icon:"4",file:"1.pdf",desc:"Nice 4to Meet You"}
-
-  ];
 }
 
 
